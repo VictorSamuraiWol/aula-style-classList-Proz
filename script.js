4e1f49025207ec90ff0560b67f407dc22fc72212
Victor Cardoso <victorclmatos@gmail.com>
2023-10-09 20:25:54 -0300

    quarta atividade js

---
+++ script.js
@@ -0,0 +1,11 @@
+const login = document.querySelector ('h2');
+console.log (login);
+login.style.color = 'yellow';
+login.style.fontSize = '50px';
+
+const button = document.querySelector ('button')
+console.log (button);
+button.style.backgroundColor = 'blue';
+button.style.borderRadius = '10px';
+button.style.borderBlockColor = 'black';
+button.style.color = 'white';
